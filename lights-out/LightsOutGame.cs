0772ce5f5e14f32203ac391ac374ddbc15a2c3ea
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-10-02 00:27:56 -0500

    Finish project

---
+++ lights-out/LightsOutGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace lights_out {
+    internal class LightsOutGame {
+        private bool[,] grid; // Stores on/off state of cells in g
+        private int numCells = 3; // Number of cells in grid
+        private readonly Random rand; // Used to generate random numbers
+
+        public LightsOutGame() {
+            rand = new Random(); // Initialize random number generator
+            grid = new bool[numCells, numCells];
+            // Turn entire grid on
+            for (var r = 0; r < numCells; r++) {
+                for (var c = 0; c < numCells; c++) {
+                    grid[r, c] = true;
+                }
+            }
+        }
+
+        // Returns the number of horizontal/vertical cells in the grid
+        public int NumCells {
+            get => numCells;
+            set { numCells = value; }
+        }
+
+        // Returns the grid value at the given row and column
+        public bool GetGridValue(int row, int col) {
+            return grid[row, col];
+        }
+
+        // Randomizes the grid
+        public void NewGame() {
+            grid = new bool[numCells, numCells];
+            // Fill grid with either white or black
+            for (var r = 0; r < numCells; r++) {
+                for (var c = 0; c < numCells; c++) {
+                    grid[r, c] = rand.Next(2) == 1;
+                }
+            }
+        }
+
+        // Inverts the selected box and all surrounding boxes
+        public void MakeMove(int row, int col) {
+            for (var i = row - 1; i <= row + 1; i++) {
+                for (var j = col - 1; j <= col + 1; j++) {
+                    if (i >= 0 && i < numCells && j >= 0 && j < numCells) {
+                        grid[i, j] = !grid[i, j];
+                    }
+                }
+            }
+        }
+
+        // Returns true if all cells are off
+        public bool PlayerWon() {
+            for (var r = 0; r < numCells; r++) {
+                for (var c = 0; c < numCells; c++) {
+                    if (grid[r, c]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- lights-out/MainWindow.xaml.cs
+++ lights-out/MainWindow.xaml.cs
@@ -18,8 +18,113 @@ namespace lights_out {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private LightsOutGame game;
+
         public MainWindow() {
             InitializeComponent();
+
+            game = new LightsOutGame();
+            InitGame();
+        }
+
+        private void InitGame() {
+            game.NewGame();
+            CreateGrid();
+        }
+
+        private void CreateGrid() {
+            int rectSize = (int) boardCanvas.Width / game.NumCells;
+            boardCanvas.Children.Clear();
+            // Create rectangles for grid
+            for (int r = 0; r < game.NumCells; r++) {
+                for (int c = 0; c < game.NumCells; c++) {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = Brushes.White;
+                    rect.Width = rectSize + 1;
+                    rect.Height = rect.Width + 1;
+                    rect.Stroke = Brushes.Black;
+                    // Store each row and col as a Point
+                    rect.Tag = new Point(r, c);
+                    // Register event handler
+                    rect.MouseLeftButtonDown += Rect_MouseLeftButtonDown;
+                    // Put the rectangle at the proper location within the canvas
+                    Canvas.SetTop(rect, r * rectSize);
+                    Canvas.SetLeft(rect, c * rectSize);
+                    // Add the new rectangle to the canvas' children
+                    boardCanvas.Children.Add(rect);
+                }
+            }
+            UpdateGridColors();
+        }
+
+        private void Rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            Rectangle rect = sender as Rectangle;
+            if (rect != null) {
+                var rowCol = (Point) rect.Tag;
+                int row = (int) rowCol.X;
+                int col = (int) rowCol.Y;
+                game.MakeMove(row, col);
+            }
+            UpdateGridColors();
+            CheckWinCondition();
+        }
+
+        private void CheckWinCondition() {
+            if (game.PlayerWon()) {
+                MessageBox.Show("You've Won!");
+                game.NewGame();
+            }
+        }
+
+        private void UpdateGridColors() {
+            int index = 0;
+
+            // Set the colors of the rectangles
+            for (int r = 0; r < game.NumCells; r++) {
+                for (int c = 0; c < game.NumCells; c++) {
+                    Rectangle rect = boardCanvas.Children[index] as Rectangle;
+                    index++;
+                    if (game.GetGridValue(r, c)) {
+                        rect.Fill = Brushes.White;
+                        rect.Stroke = Brushes.Black;
+                    }
+                    else {
+                        rect.Fill = Brushes.Black;
+                        rect.Stroke = Brushes.White;
+                    }
+                }
+            }
+        }
+
+        private void NewGame_OnClick(object sender, RoutedEventArgs e) {
+            game.NewGame();
+            UpdateGridColors();
+        }
+
+        private void Exit_OnClick(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void About_OnClick(object sender, RoutedEventArgs e) {
+            About about = new About();
+            about.ShowDialog();
+        }
+
+        private void GridSize_OnChecked(object sender, RoutedEventArgs e) {
+            MenuItem checkedMenuItem = sender as MenuItem;
+            if (checkedMenuItem != null && game != null) {
+                int newGridSize = int.Parse(checkedMenuItem.Header.ToString());
+
+                game.NumCells = newGridSize;
+                InitGame();
+
+                MenuItem checkedMenuItemParent = checkedMenuItem.Parent as MenuItem;
+                foreach (MenuItem menuItem in checkedMenuItemParent.Items) {
+                    if (menuItem != sender) {
+                        menuItem.IsChecked = false;
+                    }
+                }
+            }
         }
     }
 }
